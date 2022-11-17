cf8ee47d379b7b3dfd04c35a72839aa7af22188f
Roberzzt <robertzel_2001@yahoo.com>
2022-11-17 12:32:11 +0200

    Merge audio si video relativ sincronizat

--- UI/ui_client.py
+++ UI/ui_client.py
@@ -6,6 +6,7 @@ from queue import Queue
 import tkinter as tk
 from PIL import ImageTk, Image, ImageOps
 from typing import Tuple, Dict
+import sounddevice as sd
 
 logging.getLogger('libav').setLevel(logging.ERROR)  # removes warning: deprecated pixel format used
 
@@ -15,11 +16,13 @@ class TkinterVideo(tk.Label):
         super(TkinterVideo, self).__init__(master, *args, **kwargs)
 
         self._stream_initialized = False
-        self._load_thread = threading.Thread(target=self._display, daemon=True)
-        self._receive_thread = threading.Thread(target=self._receive_and_decode)
-        self._frame_queue = Queue(5)
-        self._audio_queue = Queue(5)
+        self._display_video_thread = threading.Thread(target=self._display_video, daemon=True)
+        self._receive_thread = threading.Thread(target=self._receive_videos, daemon=True)
+        self._play_audio_thread = threading.Thread(target=self._play_audio, daemon=True)
+        self._frame_queue = Queue()
+        self._audio_queue = Queue()
         self._current_img = None
+        self._output_audio_stream = sd.OutputStream(channels=1, samplerate=44100, dtype="float32")
 
         self._current_frame_size = (0, 0)
 
@@ -89,21 +92,19 @@ class TkinterVideo(tk.Label):
 
         self._stream_initialized = True
 
-    def _display(self):
-        """ load's file from a thread """
-
+    def _display_video(self):
         while not self._stream_initialized:
             time.sleep(0.1)
 
         while not self._frame_queue.empty():
             start = time.time()
 
-            self._current_img = self._frame_queue.get(timeout=1)
+            self._current_img = self._frame_queue.get(timeout=1).to_image()
             self.event_generate("<<FrameGenerated>>")
 
             time.sleep(max(1/self._video_info["framerate"] - time.time() + start, 0))
 
-        self._load_thread = None
+        self._display_video_thread = None
 
         try:
             self.event_generate("<<Ended>>")  # this is generated when the video ends
@@ -113,15 +114,33 @@ class TkinterVideo(tk.Label):
         self._receive_thread.join()
         print("Done")
 
+    def _play_audio(self):
+        while not self._stream_initialized:
+            time.sleep(0.1)
+
+        self._output_audio_stream.start()
+
+        while self._audio_queue.empty():
+            time.sleep(0.1)
+
+        while not self._audio_queue.empty():
+            self._output_audio_stream.write(self._audio_queue.get(block=True))
+
+        self._output_audio_stream.stop()
+
     def play(self):
-        """ plays the video file """
         if self._receive_thread is not None:
             self._receive_thread.start()
 
-        if self._load_thread is not None:
-            self._load_thread.start()
+        if self._display_video_thread is not None:
+            self._display_video_thread.start()
 
-    def _receive_and_decode(self):
+        if self._play_audio_thread is not None:
+            self._play_audio_thread.start()
+
+        self._output_audio_stream.start()
+
+    def _receive_videos(self):
         videos = ["/home/robert/Desktop/Licenta/audioVideos/1.mp4",
                   "/home/robert/Desktop/Licenta/audioVideos/2.mp4",
                   "/home/robert/Desktop/Licenta/audioVideos/3.mp4",
@@ -139,11 +158,14 @@ class TkinterVideo(tk.Label):
 
                 container.streams.video[0].thread_type = "AUTO"
                 container.discard_corrupt = True
-                for frame in container.decode(video=0):
-                    self._frame_queue.put(item=frame.to_image(), block=True)
 
-                for audio_frame in container.decode(audio=0):
-                    self._audio_queue.put(item=audio_frame, block=True)
+                for packet in container.demux():
+                    for frame in packet.decode():
+                        if isinstance(frame, av.AudioFrame):
+                            self._audio_queue.put(item=frame.to_ndarray()[0], block=True)
+                        else:
+                            self._frame_queue.put(item=frame, block=True)
+
 
     def video_info(self) -> Dict:
         """ returns dict containing duration, frame_rate, file"""
@@ -181,77 +203,15 @@ class TkinterVideo(tk.Label):
 
 
 if __name__ == "__main__":
-    # import tkinter as tk
-
     root = tk.Tk()
-    #
     videoplayer = TkinterVideo(master=root, scaled=True)
     videoplayer.pack(expand=True, fill="both")
 
-    videoplayer.play() # play the video
+    videoplayer.play()
 
     root.mainloop()
-    # s = time.time()
-    # videoplayer.receive_and_decode()
-    # end = time.time()
-    # print(end-s)
 
 
 
 
-# import av
-# import sounddevice as sd
-# import pyaudio
-# import time
-# import numpy as np
-#
-# def main():
-#     import av
-#
-#     input_container = av.open('/home/robert/Desktop/Licenta/audioVideos/1.mp4')
-#     input_stream = input_container.streams.get(audio=0)[0]
-#
-#     output_container = av.open('live_stream.mp3', 'w')
-#     output_stream = output_container.add_stream('mp3')
-#
-#     for frame in input_container.decode(input_stream):
-#         #frame.pts = None
-#         for packet in output_stream.encode(frame):
-#             output_container.mux(packet)
-#
-#     for packet in output_stream.encode(None):
-#         output_container.mux(packet)
-#
-#     output_container.close()
-#
-# if __name__ == "__main__":
-#     videos = ["/home/robert/Desktop/Licenta/audioVideos/1.mp4",
-#               "/home/robert/Desktop/Licenta/audioVideos/2.mp4",
-#               "/home/robert/Desktop/Licenta/audioVideos/3.mp4",
-#               "/home/robert/Desktop/Licenta/audioVideos/4.mp4",
-#               "/home/robert/Desktop/Licenta/audioVideos/5.mp4",
-#               "/home/robert/Desktop/Licenta/audioVideos/6.mp4",
-#               "/home/robert/Desktop/Licenta/audioVideos/7.mp4",
-#               "/home/robert/Desktop/Licenta/audioVideos/8.mp4",
-#               "/home/robert/Desktop/Licenta/audioVideos/9.mp4"]
-#
-#     stream = sd.OutputStream(channels=1, samplerate=44100, dtype="float32")
-#
-#     all_stream = np.array([], dtype=np.float32)
-#     for video in videos:
-#         with av.open(video) as container:
-#             container.streams.video[0].thread_type = "AUTO"
-#             container.discard_corrupt = True
-#             for frame in container.decode(container.streams.audio[0]):
-#                 all_stream = np.append(all_stream, frame.to_ndarray()[0])
-#                 #sd.play(data=frame.to_ndarray()[0], samplerate=44100, blocking=True)
-#
-#     c = 0
-#     stream.start()
-#     s = time.time()
-#     while c < len(all_stream):
-#         #sd.play(data=all_stream.tolist()[c:c+1470 * 2], samplerate=44100, blocking=True)
-#         stream.write(all_stream[c:c+1024])
-#         c += 1024
-#
-#     print(time.time() - s)
+
